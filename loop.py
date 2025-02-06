f52121cf5461c87dce28b473cd73265e52d84224
admiral <kartikeyk1776@gmail.com>
2025-02-06 23:05:46 +0530

    about loop

---
+++ loop.py
@@ -0,0 +1,17 @@
+x = range(100)
+for i in x:
+    print(i)
+
+
+y = ["jos", "ben", "kl", "gill", "malinga", "murlitharan", "rishabh pant"]
+for n in y:
+    print(n)
+
+
+num = [1,6,3,7,247,99,54,87]
+largest = num[0]
+for x in num:
+    if x > largest:
+        largest = x
+
+print(largest)

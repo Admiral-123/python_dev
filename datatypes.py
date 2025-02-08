9cc391a75390f5fd0885309ae464519db9802729
admiral <kartikeyk1776@gmail.com>
2025-02-08 22:16:27 +0530

    datatypes 1

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+tup = tuple()
+
+print(tup)
+
+tup2 = ('k', "lkj", 55, 7.23)
+print(tup2)
+
+listEx = [9, 4, 59]
+listConvertedtoTup = tuple(listEx)
+print(listConvertedtoTup)
+
+dictionaryEx = {1: "wow", 2: "momo", 3: "pizza"}
+print(dictionaryEx.get(2))
+dictConvertedToTup = list(dictionaryEx)
+print(dictConvertedToTup)
